a4b5df8607359781153648474513bb542bdf7313
Anne Sallaska <alsallaska@gmail.com>
2025-05-07 21:00:28 -0700

    updates due to changing how apply categories to db

--- data_processing/category_scraping.py
+++ data_processing/category_scraping.py
@@ -6,8 +6,8 @@ from sqlalchemy import text
 
 engine = get_engine()
 
-df = pd.read_csv("data/categories.csv")
-df['subcategory'] = df.categories.apply(lambda x: x.split(', '))
+df = pd.read_csv("data/categories_hash.csv")
+df['subcategory'] = df.categories.apply(lambda x: x.split(','))
 df = df.explode('subcategory')
 
 with engine.begin() as txn:
@@ -15,20 +15,24 @@ with engine.begin() as txn:
 
     txn.execute(text("""
            CREATE TEMPORARY TABLE tmp_incident_category (   
-           incident_id uuid,
+           description_hash character varying,
            subcategory character varying)
        """))
 
-    df[['incident_id', 'subcategory']].to_sql("tmp_incident_category", txn, if_exists='append',
+    df[['description_hash', 'subcategory']].to_sql("tmp_incident_category", txn, if_exists='append',
                                               index=False, chunksize=500, method='multi')
 
     r = txn.execute(text("""
         INSERT INTO incident.incident_category
         (incident_id, subcategory)
-        SELECT incident_id, subcategory
-        FROM tmp_incident_category
+        SELECT i.incident_id, t.subcategory
+        FROM tmp_incident_category t 
+        JOIN incident.incident i on i.description_hash = t.description_hash
     """))
 
     logging.info(f"Rows upserted: {r.rowcount}")
 
     txn.execute(text("""DROP TABLE IF EXISTS tmp_incident_category;"""))
+
+
+    
